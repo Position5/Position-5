8dc0a733ba6916f7ef96ac6bcc1b283384062546
Arpit Choudhary <arpitkumar147@gmail.com>
2021-02-15 22:07:06 +0530

    allo world

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Position-5
 
 Dedicated to best position 5 support.
+
+## Setting Up
+
+- Install Python 3.5+
+- Install pip packages: `pip install -r requirements.txt`
+- Create a `.env` file in this directory, and place token `DISCORD_BOT_TOKEN=token-goes-here`
+- Run `python postion5`
--- position5/__main__.py
+++ position5/__main__.py
@@ -1,6 +1,19 @@
-def main():
-    ...
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
 
-if __name__ == '__main__':
-    print('Test')
-    main()
+load_dotenv('.env')
+bot = commands.Bot(command_prefix='.')
+
+
+@bot.event
+async def on_ready():
+    print('Connected to bot: {}'.format(bot.user.name))
+    print('Bot ID: {}'.format(bot.user.id))
+
+
+@bot.command()
+async def helloworld(ctx):
+    await ctx.send('Allo World!')
+
+bot.run(os.getenv('DISCORD_BOT_TOKEN'))
