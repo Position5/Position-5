ef02f20390e48c20dcaea8d9a99a07a8be5bad71
Arpit Choudhary <arpitkumar147@gmail.com>
2021-02-16 21:32:25 +0530

    add two files from web xd

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Dedicated to best position 5 support.
 - Install Python 3.5+
 - Install pip packages: `pip install -r requirements.txt`
 - Create a `.env` file in this directory, and place token `DISCORD_BOT_TOKEN=token-goes-here`
-- Run `python postion5`
+- Run `python position5`
--- position5/__main__.py
+++ position5/__main__.py
@@ -1,19 +1,42 @@
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+import replit
+import keep_alive
 
 load_dotenv('.env')
-bot = commands.Bot(command_prefix='.')
 
+
+def get_prefix(client, message):
+    # sets the prefixes, u can keep it as an array of only 1 item if you need only one prefix
+    prefixes = ['.']
+
+    if not message.guild:
+        prefixes = ['==']   # Only allow '==' as a prefix when in DMs
+
+    # Allow users to @mention the bot instead of using a prefix when using a command.
+    return commands.when_mentioned_or(*prefixes)(client, message)
+
+
+bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True)
+
+
+cogs = ['cogs.basic','cogs.embed']
 
 @bot.event
 async def on_ready():
+    replit.clear()
     print('Connected to bot: {}'.format(bot.user.name))
     print('Bot ID: {}'.format(bot.user.id))
+    bot.remove_command('help')
+    for cog in cogs:
+        bot.load_extension(cog)
 
 
 @bot.command()
 async def helloworld(ctx):
     await ctx.send('Allo World!')
 
-bot.run(os.getenv('DISCORD_BOT_TOKEN'))
+keep_alive.keep_alive()
+
+bot.run(os.getenv('DISCORD_BOT_TOKEN'), bot=True, reconnect=True)
