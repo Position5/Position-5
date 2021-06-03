1392021f56d806420da9efb9c9d0da272546ef2e
Arpit Choudhary <arpitkumar147@gmail.com>
2021-06-03 08:22:01 +0530

    add param to give url, dota

--- position5/cogs/liquipedia.py
+++ position5/cogs/liquipedia.py
@@ -13,31 +13,33 @@ class Liquipedia(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(name="animajor", description="animajor schedule", aliases=["major"])
+    @commands.command(name="dota", description="dota schedule", aliases=["animajor", "major"])
     @delete_message()
     @log_params()
-    async def animajor(self, ctx: commands.Context):
-        url = "https://liquipedia.net/dota2/WePlay/AniMajor/2021"
+    async def dota_schedule(self, ctx: commands.Context, *, url: str = ""):
+        if not url:
+            url = "https://liquipedia.net/dota2/WePlay/AniMajor/2021"
         _resp = requests.get(url)
         soup = BeautifulSoup(_resp.text, "html.parser")
 
         upcoming_matches = soup.find(string="Upcoming Matches")
         tables = upcoming_matches.find_parent("div").find_parent("div").find_parent("div").find_all("table")
 
-        title = "Animajor 2021"
+        title = soup.title.text.split("-")[0].strip()
         description = ""
 
         for table in tables:
             for tbody in table.find_all("tbody"):
                 rows = tbody.find_all("tr")
                 teams = rows[0].find_all("td")
-                stream = rows[1].td
-                time = parser.parse(stream.span.span.text) + timedelta(hours=5, minutes=30)
+                stream_info = rows[1].td
+                time = parser.parse(stream_info.span.span.text) + timedelta(hours=5, minutes=30)
 
-                description += f"{stream.div.div.text}\n"
-                description += f"{teams[0].span.span.a['title']} {teams[1].text} {teams[2].span.span.a['title']}\n"
+                description += f"{stream_info.div.div.text}\n"
+                description += f"{teams[0].span['data-highlightingclass']}"
+                description += f" {teams[1].text} {teams[2].span['data-highlightingclass']}\n"
                 description += f"Time: {time.strftime('%b %d, %Y %H:%M')}\n"
-                description += f"Watch on: https://twitch.tv/{stream.span.span['data-stream-twitch']}\n\n"
+                description += f"Watch on: https://twitch.tv/{stream_info.span.span['data-stream-twitch']}\n\n"
 
         await ctx.send(
             embed=discord.Embed(title=title, description=description)
