3074e6a4c585d0a11de823a7ce5eb3d98b9c4203
Arpit Choudhary <arpitkumar147@gmail.com>
2021-06-05 10:09:30 +0530

    add glitch and glitchp

--- position5/cogs/pic.py
+++ position5/cogs/pic.py
@@ -2,6 +2,7 @@ from io import BytesIO
 from PIL import Image
 import discord
 from discord.ext import commands
+from glitch_this import ImageGlitcher
 from . import delete_message, log_params, EMOTES_PATH, PIC_PATH, TEMP_PATH
 
 
@@ -10,6 +11,7 @@ class Pic(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.glitcher = ImageGlitcher()
 
     @commands.command(name="xdoubt", description="xdoubt pic")
     @delete_message()
@@ -28,6 +30,26 @@ class Pic(commands.Cog):
         template.paste(Image.open(BytesIO(await author.read())), (1005, 732))
         template.save(f"{TEMP_PATH}completed.png")
         await ctx.send(file=discord.File(f"{TEMP_PATH}completed.png"))
+
+    @commands.command(name="glitch", description="glitch image color", usage="upload image(float 0-10)")
+    @delete_message()
+    @log_params()
+    async def glitch_color(self, ctx: commands.Context, amount: float = 2.718):
+        for att in ctx.message.attachments:
+            image = Image.open(BytesIO(await att.read()))
+            glitch_img = self.glitcher.glitch_image(image, amount, color_offset=True)
+            glitch_img.save(f"{TEMP_PATH}glitch.png")
+            await ctx.send(file=discord.File(f"{TEMP_PATH}glitch.png"))
+
+    @commands.command(name="glitchp", description="glitch image position", usage="upload image(float 0-10)")
+    @delete_message()
+    @log_params()
+    async def glitch_position(self, ctx: commands.Context, amount: float = 2.718):
+        for att in ctx.message.attachments:
+            image = Image.open(BytesIO(await att.read()))
+            glitch_img = self.glitcher.glitch_image(image, amount, color_offset=False)
+            glitch_img.save(f"{TEMP_PATH}glitch.png")
+            await ctx.send(file=discord.File(f"{TEMP_PATH}glitch.png"))
 
 
 def setup(bot):
